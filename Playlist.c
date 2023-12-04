67d63ebda491f762bcddb8e8a3a8ed2d345e7449
João Marcos <joao.mcruz@a.ucb.br>
2023-12-04 14:38:06 -0300

    Adicionando Alocação Dinâmica/Ajustes

--- Playlist.c
+++ Playlist.c
@@ -1,8 +1,7 @@
-#include <stdio.h> //fiz só esse comecinho do jeito que a gnt combinou pra ficar mais fácil
+// fiz só esse comecinho do jeito que a gnt combinou pra ficar mais fácil
+#include <stdio.h>
 #include <string.h>
-
-#define MAX_MUSICAS 100
-#define MAX_NOME 50
+#include <stdlib.h>
 
 typedef struct Musica
 {
@@ -12,41 +11,45 @@ typedef struct Musica
 } Musica;
 
 // Botei essa primeira parte de colocar música na playlist,
-// Quem for revisar quando tiver completo avisa se deu bom lá no grupo
-void inserirMusica(Musica playlist[], int *numMusicas)
+// Agora a playlist é um ponteiro
+void inserirMusica(struct Musica **playlist, int *numMusicas, int *tamanhoPlaylist)
 {
-    if (*numMusicas < MAX_MUSICAS)
+    // Se a playlist estiver cheia, realocamos mais espaço
+    if (*numMusicas == *tamanhoPlaylist)
     {
-        Musica novaMusica;
-
-        printf("Digite o titulo da musica: ");
-        scanf(" %[^\n]", novaMusica.titulo);
-
-        printf("Digite o nome do artista: ");
-        scanf(" %[^\n]", novaMusica.artista);
-
-        novaMusica.favorita = 0; // De começo ela não favorita aí tem que executar a outra função de favoritar, quem for fazer.
-                                            // Presta atenção aqui !!!
-
-        playlist[*numMusicas] = novaMusica;
-        (*numMusicas)++;
-
-        printf("Musica inserida com sucesso!\n");
+        *tamanhoPlaylist *= 2; // Dobramos o tamanho da playlist
+        *playlist = realloc(*playlist, *tamanhoPlaylist * sizeof(struct Musica));
+        if (*playlist == NULL)
+        {
+            printf("Erro ao alocar memoria.\n");
+            return;
+        }
     }
-    else
-    {
-        printf("A playlist esta cheia. Remova musicas antes de adicionar novas.\n");
-    }
+
+    struct Musica novaMusica;
+
+    printf("Digite o titulo da musica: ");
+    scanf(" %[^\n]", novaMusica.titulo);
+
+    printf("Digite o nome do artista: ");
+    scanf(" %[^\n]", novaMusica.artista);
+
+    novaMusica.favorita = 0;
+
+    (*playlist)[*numMusicas] = novaMusica;
+    (*numMusicas)++;
+
+    printf("Musica inserida com sucesso!\n");
 }
 
 // oiii gente, coloquei do jeito que a gnt combinou, minha parte de função, só pra mostrar msm!!
-void mostrarPlaylist(Musica playlist[], int numMusicas)
+void mostrarPlaylist(struct Musica **playlist, int numMusicas)
 {
     printf("\nPlaylist:\n");
     for (int i = 0; i < numMusicas; i++)
     {
-        printf("%d. %s - %s", i + 1, playlist[i].artista, playlist[i].titulo);
-        if (playlist[i].favorita)
+        printf("%d. %s - %s", i + 1, (*playlist)[i].artista, (*playlist)[i].titulo);
+        if ((*playlist)[i].favorita)
         {
             printf(" (Favoritada)");
         }
@@ -54,8 +57,8 @@ void mostrarPlaylist(Musica playlist[], int numMusicas)
     }
 }
 
-// Essa aqui só vai buscar a música na playlist, o professor não falou como, aí busca pelo nome e pelo autor
-void buscarMusica(Musica playlist[], int numMusicas)
+// Essa aqui só vai buscar a música na playlist.
+void buscarMusica(struct Musica **playlist, int numMusicas)
 {
     char busca[50];
     printf("Digite o titulo ou artista da musica a ser buscada: ");
@@ -64,10 +67,10 @@ void buscarMusica(Musica playlist[], int numMusicas)
     printf("\nResultados da busca:\n");
     for (int i = 0; i < numMusicas; i++)
     {
-        if (strstr(playlist[i].titulo, busca) || strstr(playlist[i].artista, busca))
+        if (strstr((*playlist)[i].titulo, busca) || strstr((*playlist)[i].artista, busca))
         {
-            printf("%d. %s - %s", i + 1, playlist[i].artista, playlist[i].titulo);
-            if (playlist[i].favorita)
+            printf("%d. %s - %s", i + 1, (*playlist)[i].artista, (*playlist)[i].titulo);
+            if ((*playlist)[i].favorita)
             {
                 printf(" (Favorita)");
             }
@@ -77,8 +80,9 @@ void buscarMusica(Musica playlist[], int numMusicas)
 }
 
 // Função que vai editar as informações das músicas
-void editarMusica(Musica playlist[], int numMusicas)
+void editarMusica(struct Musica **playlist, int numMusicas)
 {
+
     int indice;
     printf("Digite o numero da musica a ser editada: ");
     scanf("%d", &indice);
@@ -88,21 +92,21 @@ void editarMusica(Musica playlist[], int numMusicas)
     {
 
         printf("Digite o novo titulo da musica: ");
-        scanf(" %[^\n]", playlist[indice - 1].titulo);
+        scanf(" %[^\n]", (*playlist)[indice - 1].titulo);
 
         printf("Digite o novo nome do artista: ");
-        scanf(" %[^\n]", playlist[indice - 1].artista);
+        scanf(" %[^\n]", (*playlist)[indice - 1].artista);
 
         printf("Informacoes da musica editadas com sucesso!\n");
     }
     else
     {
-        printf("Numero invalido.\n");
+        printf("Numero invalido. Tente novamente.\n");
     }
 }
 
 // Exclui a música que quiser
-void removerMusica(Musica playlist[], int *numMusicas)
+void removerMusica(struct Musica **playlist, int *numMusicas)
 {
     int indice;
     printf("Digite o numero da musica a ser removida: ");
@@ -112,19 +116,19 @@ void removerMusica(Musica playlist[], int *numMusicas)
     {
         for (int i = indice - 1; i < *numMusicas - 1; i++)
         {
-            playlist[i] = playlist[i + 1];
+            (*playlist)[i] = (*playlist)[i + 1];
         }
         (*numMusicas)--;
         printf("Musica removida com sucesso!\n");
     }
     else
     {
-        printf("Numero invalido.\n");
+        printf("Numero invalido. Tente novamente.\n");
     }
 }
 
-// Talvez favorite a música, tem que testar :(
-void favoritarMusica(Musica playlist[], int numMusicas)
+// Função que vai favoritar as músicas
+void favoritarMusica(struct Musica **playlist, int numMusicas)
 {
     int indice;
     printf("Digite o numero da musica a ser favoritada/desfavoritada: ");
@@ -132,8 +136,8 @@ void favoritarMusica(Musica playlist[], int numMusicas)
 
     if (indice >= 1 && indice <= numMusicas)
     {
-        playlist[indice - 1].favorita = !playlist[indice - 1].favorita;
-        if (playlist[indice - 1].favorita)
+        (*playlist)[indice - 1].favorita = !(*playlist)[indice - 1].favorita;
+        if ((*playlist)[indice - 1].favorita)
         {
             printf("Musica favoritada com sucesso!\n");
         }
@@ -144,12 +148,12 @@ void favoritarMusica(Musica playlist[], int numMusicas)
     }
     else
     {
-        printf("Numero invalido.\n");
+        printf("Numero invalido. Tente novamente.\n");
     }
 }
 
-// Parte difícil... salva a playlist na pasta output dentro de onde o arquivo todo ta, demorei mas achei onde salva
-void salvarPlaylist(Musica playlist[], int numMusicas, char nomePessoa[])
+// Parte difícil... salva a playlist na pasta output dentro de onde o arquivo todo ta.
+void salvarPlaylist(struct Musica **playlist, int numMusicas, char nomePessoa[])
 {
     char nomeArquivo[100]; // Tamanho arbitrário para garantir espaço suficiente
     sprintf(nomeArquivo, "Playlist de %s.txt", nomePessoa);
@@ -160,7 +164,7 @@ void salvarPlaylist(Musica playlist[], int numMusicas, char nomePessoa[])
     {
         for (int i = 0; i < numMusicas; i++)
         {
-            fprintf(arquivo, "%s - %s - %d\n", playlist[i].titulo, playlist[i].artista, playlist[i].favorita);
+            fprintf(arquivo, "%s - %s - %d\n", (*playlist)[i].titulo, (*playlist)[i].artista, (*playlist)[i].favorita);
         }
 
         fclose(arquivo);
@@ -173,14 +177,21 @@ void salvarPlaylist(Musica playlist[], int numMusicas, char nomePessoa[])
     }
 }
 
-// Só juntei tudo e tentei incluir tudo, acho que foi.
+// Só juntei tudo e tentei incluir tudo.
 int main()
 {
-    Musica playlist[MAX_MUSICAS];
+    int tamanhoPlaylist = 10; // Começamos com espaço para 10 músicas
+    
+    struct Musica *playlist = malloc(tamanhoPlaylist * sizeof(struct Musica));
+    if (playlist == NULL)
+    {
+        printf("Erro ao alocar memoria.\n");
+        return 1;
+    }
     int numMusicas = 0;
     int opcao;
 
-    char nomePessoa[MAX_NOME];
+    char nomePessoa[50];
 
     printf("Digite o seu nome: ");
     scanf(" %[^\n]", nomePessoa);
@@ -206,34 +217,35 @@ int main()
         switch (opcao)
         {
         case 1:
-            inserirMusica(playlist, &numMusicas);
+            inserirMusica(&playlist, &numMusicas, &tamanhoPlaylist);
             break;
         case 2:
-            mostrarPlaylist(playlist, numMusicas);
+            mostrarPlaylist(&playlist, numMusicas);
             break;
         case 3:
-            buscarMusica(playlist, numMusicas);
+            buscarMusica(&playlist, numMusicas);
             break;
         case 4:
-            editarMusica(playlist, numMusicas);
+            editarMusica(&playlist, numMusicas);
             break;
         case 5:
-            removerMusica(playlist, &numMusicas);
+            removerMusica(&playlist, &numMusicas);
             break;
         case 6:
-            favoritarMusica(playlist, numMusicas);
+            favoritarMusica(&playlist, numMusicas);
             break;
         case 7:
-            salvarPlaylist(playlist, numMusicas, nomePessoa);
+            salvarPlaylist(&playlist, numMusicas, nomePessoa);
             break;
         case 8:
             printf("Saindo do programa. Ate mais!\n");
-
             break;
         default:
             printf("Opcao invalida. Tente novamente.\n");
         }
     } while (opcao != 8);
+
+    free(playlist); // Liberando memória
 
     return 0;
 }
